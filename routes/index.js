1971703276db2bfd84cd4323d96d366c93b4d25b
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-03-18 17:49:08 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user'); 
+var mongoose = require('mongoose');
+
+// Download the Node helper library from twilio.com/docs/node/install
+// These vars are your accountSid and authToken from twilio.com/user/account
+var accountSid = 'ACf3f47245558bab8ef0ad24f5f4c464d0';
+var authToken = "f5e95817975c0d7d9635ca0e14058113";
+var client = require('twilio')(accountSid, authToken);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
